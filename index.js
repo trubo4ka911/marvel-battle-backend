05b41e66edeeebce656281aab28dafbdf87c0ba4
Anna Trubnikova <47085312+trubo4ka911@users.noreply.github.com>
2024-06-19 13:44:02 +0100

    styles fix

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
